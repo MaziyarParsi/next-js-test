ccd42e52b95b2b453952db470a9b6d6f89060ac9
maziyar parsi <mparsi1992@gmail.com>
2021-11-10 15:10:01 +0330

    add-pages

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+const About = () => {
+	return (
+		<div>
+			<h2>this is about page</h2>
+			<p>
+				Elit labore sint incididunt eiusmod. Minim proident ullamco qui est
+				eiusmod est eu ullamco. Non non proident culpa dolor dolor deserunt
+				amet laboris qui anim aute esse. Consectetur commodo pariatur ipsum
+				nostrud deserunt consectetur ad dolor sit aliqua. Sunt aliquip sit
+				excepteur cupidatat qui sunt.
+			</p>
+		</div>
+	)
+}
+
+export default About
---
+++ pages/ninjas/test.js
@@ -0,0 +1,9 @@
+const Test = () => {
+	return (
+		<div>
+			<h1>All ninjas</h1>
+		</div>
+	)
+}
+
+export default Test
