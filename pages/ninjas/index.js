e03c4335e2e822a688b1709516c57cc7f3d1256e
maziyar parsi <mparsi1992@gmail.com>
2021-11-10 18:28:52 +0330

    add-items

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import style from '../../styles/ninjas.module.css'
+import Link from 'next/link'
 
 export const getStaticProps = async () => {
 	const response = await fetch('https://jsonplaceholder.typicode.com/users')
@@ -21,11 +22,11 @@ const Ninjas = ({ ninjas }) => {
 			<div>
 				<h1>All ninjas</h1>
 				{ninjas.map((ninja) => (
-					<div key={ninja.id}>
+					<Link key={ninja.id} href={`/ninjas/${ninja.id}`}>
 						<a className={style.single}>
 							<h3>{ninja.name}</h3>
 						</a>
-					</div>
+					</Link>
 				))}
 			</div>
 		</>
